4b40b4d51718312459d40c897d87122bca4cd314
chooblarin <choo.bla.rin@gmail.com>
2017-08-17 22:23:43 +0900

    Check echigo line status

--- lib/nvision.ex
+++ lib/nvision.ex
@@ -4,15 +4,18 @@ defmodule Nvision do
   """
 
   @doc """
-  Hello world.
+  Start app.
 
   ## Examples
 
-      iex> Nvision.hello
-      :world
+      iex> Nvision.check
 
   """
-  def hello do
-    :world
+  def check do
+    IO.puts "starting"
+    ret = HTTPoison.get! "http://traininfo.jreast.co.jp/train_info/shinetsu.aspx"
+    %HTTPoison.Response{body: body} = ret
+    [status] = Floki.attribute(body, "th:fl-contains('越後線') + td.acess_i img", "alt")
+    IO.puts status
   end
 end
